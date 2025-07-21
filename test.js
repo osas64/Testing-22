9399afa883d1d4f90d08f9ce886dbe681248ec7d
osas64 <chartroom2@gmail.com>
2025-07-21 03:11:42 +0100

    add some code for testing

--- test.js
+++ test.js
@@ -1,2 +1,17 @@
-const me = "Hello";
-console.log(me);
+
+let balance = 0;
+let num = [];
+
+
+
+function deposit(number, amount) {
+    if(balance >= 0) {
+        balance += amount;
+        num.push(number);
+        console.log(balance);
+        console.log(num);
+    }
+}
+
+deposit(83737377, 4);
+
